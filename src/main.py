e826208c399e1bc1863882e52e09590e14c85208
CSneko <minecrafthetun@outlook.com>
2024-03-20 23:00:43 +0800

    添加更多宠物支持

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ import sys
 from PyQt5.QtWidgets import *
 import pet
 
+
+
+
 class PetListDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -36,19 +39,20 @@ class PetDialog(QDialog):
                     file_path = os.path.join(root, file_name)
                     with open(file_path, 'r') as file:
                         data = json.load(file)
-                        # 处理宠物数据
                         button = QPushButton(data['name'])
-                        button.clicked.connect(lambda: create_pet(file_path))
+                        button.clicked.connect(lambda _, file_path=file_path: create_pet(file_path))  # 传递file_path参数
                         layout.addWidget(button)
         self.setLayout(layout)
 
 def create_pet(file_path):
+    app = QApplication(sys.argv)
     with open(file_path, 'r') as file:
         data = json.load(file)
         if data["type"] == "gif":
-            widget = pet.GifWidget(data['path'])
+            widget = pet.GifWidget(data)
 
             widget.show()
+
             sys.exit(app.exec_())
         else:
             print("无效的json")
@@ -60,18 +64,17 @@ def create_main_ui():
 
     layout = QVBoxLayout()
     button1 = QPushButton('宠物列表')
-    button1.clicked.connect(lambda: show_pet_list_dialog())
+    button1.clicked.connect(show_pet_list_dialog)
     layout.addWidget(button1)
 
     window.setLayout(layout)
     window.show()
     sys.exit(app.exec_())
 
-
 def show_pet_list_dialog():
     dialog = PetListDialog()
     dialog.exec_()
 
-
 if __name__ == '__main__':
     create_main_ui()
+
--- src/pet.py
+++ src/pet.py
@@ -3,28 +3,34 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+
 class GifWidget(QWidget):
-    def __init__(self, gif_path):
+    def __init__(self, data):
         super().__init__()
-        self.initUI(gif_path)
+        self.initUI(data)
 
     def quit(self):
         self.close()
         QApplication.quit()
 
-    def initUI(self, gif_path):
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.SubWindow)  # |Qt.WindowStaysOnTopHint 置顶
+    def initUI(self, data):
+        gif_path = data['path']
+        size_x = data['size']['x']
+        size_y = data['size']['y']
+        speed = data['speed']
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.SubWindow)
         self.setAutoFillBackground(False)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.repaint()
 
         layout = QVBoxLayout()
         self.label = QLabel()
+        self.label.setScaledContents(True)  # 设置缩放内容以填充整个空间
         layout.addWidget(self.label)
         self.setLayout(layout)
 
         self.movie = QMovie(gif_path)
-        self.movie.setSpeed(400)
+        self.movie.setSpeed(speed)
         self.label.setMovie(self.movie)
         self.movie.start()
 
@@ -38,8 +44,11 @@ class GifWidget(QWidget):
         self.tray_icon.setContextMenu(self.tray_icon_menu)
         self.tray_icon.show()
 
-        # 设置窗口大小为60x60
-        self.setGeometry(100, 100, 60, 60)
+        self.setFixedSize(size_x, size_y)  # 设置初始窗口大小
+
+    def resizeEvent(self, event):
+        size = event.size()
+        self.label.setFixedSize(size)  # 在窗口大小变化时，调整 GIF 图片的大小以适应窗口
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
